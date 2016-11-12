1a1c2a825a63243f170df5b458f3f32fef3c21a7
Alexander Schrab <alexander.schrab@gmail.com>
2016-11-12 16:32:06 +0100

    go: Update client

--- go/client/client.go
+++ go/client/client.go
@@ -10,19 +10,14 @@ import (
 	pb "github.com/screaming-goat/globalwarmingfactorial/go/factorial"
 )
 
-const (
-	address     = ":36215"
-	defaultName = "world"
-)
-
 func main() {
-	var serverflag = flag.String("server", "localhost", "factorial server")
+	var serverflag = flag.String("server", "localhost:36215", "factorial server")
 	var nflag = flag.Int64("n", 1, "input value to factorial function")
 
 	flag.Parse()
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial((*serverflag) + address, grpc.WithInsecure())
+	conn, err := grpc.Dial(*serverflag, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
